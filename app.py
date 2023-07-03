63e37a1f8bade5c2d07e2a64957700d4f9ccb0ee
alireza gholami <gholamialireza1004@gmail.com>
2023-07-03 11:21:58 +0330

    add show_meet , add_mark_professor, add_mark_student,  remove_section

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ import MySQLdb
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'university'
-app.config['MYSQL_HOST'] = 'localhost'
-mysql = MySQL(app)
-
-
 def login_check_student(data) -> bool:
     try:
         username = data['username']
@@ -35,13 +26,14 @@ def login_check_professor(data) -> bool:
         password = data['password']
         cur = mysql.connection.cursor()
         cur.execute(f'''SELECT * FROM professor
-            WHERE idProfessor = {username} AND password = {password}''')
+            WHERE idProfessor = {username} AND password = "{password}"''')
         account = cur.fetchone()
         if account:
             return True
         else:
             return False
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -88,6 +80,111 @@ def exam_schedule():
     username = data['username']
     cur.execute(f'''SELECT e.section_idsection, e.Exam_date, e.Exam_time FROM student s, student_has_exam she, exam e
                     where s.ssn = {username} and s.ssn = she.Student_ssn and e.idExam = she.Exam_idExam ;''')
+
+    rv = cur.fetchall()
+    print(rv)
+    return dumps(rv, default=str)
+
+
+@app.route('/add_professor_mark', methods=['POST'])
+def add_professor_mark():
+    data = request.get_json()
+    if not login_check_student(data):
+        msg = 'Incorrect username / password !'
+        return {'msg': msg}, 401
+    cur = mysql.connection.cursor()
+    username = data['username']
+    idsection = data['idsection']
+    professor_mark = data['professor_mark']
+    cur.execute(f'''SELECT distinct ss.* FROM student stu, student_has_section ss, term t, section s
+            WHERE stu.ssn=ss.student_ssn AND ss.Student_ssn = {username} AND ss.Section_idSection = {idsection} AND ss.is_removerd = 0 
+            AND t.idterm = s.Term_idTerm AND t.is_current = 1''')
+    account = cur.fetchone()
+    if account:
+        try:
+            cur.execute(
+                f'''UPDATE student_has_section SET professor_mark = {professor_mark} WHERE Student_ssn = {username} AND Section_idSection = {idsection}''')
+            mysql.connection.commit()
+            return {'status': 'success'}
+        except Exception as e:
+            print(e)
+            return {'status': 'unsuccessful'}
+    else:
+        msg = 'You are not the student of this section or section is removed or section is not in the current term!'
+        return {'msg': msg}, 401
+
+
+@app.route('/add_student_mark', methods=['POST'])
+def add_student_mark():
+    data = request.get_json()
+    if not login_check_professor(data):
+        msg = 'Incorrect username / password !'
+        return {'msg': msg}, 401
+    cur = mysql.connection.cursor()
+    username = data['username']
+    idsection = data['idsection']
+    student_ssn = data['student_ssn']
+    student_mark = data['student_mark']
+    cur.execute(f'''SELECT distinct ss.* FROM section s, student_has_section ss, professor p,  term t
+            WHERE s.Professor_idProfessor = p.idprofessor AND s.Professor_idProfessor = {username} AND s.idsection = {idsection}
+            AND ss.Section_idSection = {idsection} AND ss.is_signed = 0 AND ss.is_removerd = 0
+            AND t.is_current = 1''')
+    account = cur.fetchone()
+    if account:
+        try:
+            cur.execute(
+                f'''UPDATE student_has_section SET student_mark = {student_mark} WHERE Student_ssn = {student_ssn} AND Section_idSection = {idsection};''')
+            mysql.connection.commit()
+            cur.execute(
+                f'''UPDATE student_has_section SET student_mark = {student_mark} WHERE Student_ssn = {student_ssn} AND Section_idSection = {idsection};''')
+            mysql.connection.commit()
+            return {'status': 'success'}
+        except Exception as e:
+            print(e)
+            return {'status': 'unsuccessful'}
+    else:
+        msg = 'You are not the professor of this section  or section is removed or section is not in the current term!'
+        return {'msg': msg}, 401
+
+@app.route('/remove_section', methods=['POST'])
+def remove_section():
+    data = request.get_json()
+    if not login_check_student(data):
+        msg = 'Incorrect username / password !'
+        return {'msg': msg}, 401
+    cur = mysql.connection.cursor()
+    username = data['username']
+    idsection = data['idsection']
+
+    cur.execute(f'''SELECT distinct ss.* FROM student stu, student_has_section ss, term t, section s
+            WHERE stu.ssn=ss.student_ssn AND ss.Student_ssn = {username} AND ss.Section_idSection = {idsection} AND ss.is_removerd = 0 
+            AND t.idterm = s.Term_idTerm AND t.is_current = 1''')
+    section_student = cur.fetchone()
+    if section_student:
+        try:
+            cur.execute(
+                f'''UPDATE student_has_section SET is_removerd = 1 WHERE Student_ssn = {username} AND Section_idSection = {idsection}''')
+            mysql.connection.commit()
+            return {'status': 'success'}
+        except Exception as e:
+            print(e)
+            return {'status': 'unsuccessful'}
+    else:
+        msg = 'You are not the student of this section or section is removed or section is not in the current term!'
+        return {'msg': msg}, 401
+
+@app.route('/show_meet', methods=['GET'])
+def show_meet():
+    data = request.get_json()
+    if not login_check_student(data):
+        msg = 'Incorrect username / password !'
+        return {'msg': msg}, 401
+    cur = mysql.connection.cursor()
+    username = data['username']
+    cur.execute(f'''SELECT c.Course_name, count(m.Student_ssn) as Number_of_absences
+                    FROM meet m, section s, course c
+                    where m.Student_ssn = {username} and s.idsection = m.Section_idSection and s.Course_idCourse = c.idcourse
+                    group by c.Course_name;''')
 
     rv = cur.fetchall()
     print(rv)
